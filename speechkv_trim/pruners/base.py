833a4ca74dc82544a13c982725b8dde28d327cfb
Chen Zhuoran <256720182+jelllott@users.noreply.github.com>
2023-05-22 20:05:22 +0800

    switch recency to score+select+apply pattern

--- speechkv_trim/pruners/base.py
+++ speechkv_trim/pruners/base.py
@@ -1,6 +1,18 @@
 """Base pruner."""
+import torch
+
 class BasePruner:
     def __init__(self, budget):
-        self.budget = budget
-    def __call__(self, k, v, attn, meta):
+        self.budget = int(budget)
+    def score(self, k, v, attn, meta):
         raise NotImplementedError
+    def select(self, scores, budget):
+        _, idx = torch.topk(scores, budget)
+        idx, _ = torch.sort(idx)
+        return idx
+    def apply(self, k, v, idx):
+        return k.index_select(2, idx), v.index_select(2, idx)
+    def __call__(self, k, v, attn, meta):
+        s = self.score(k, v, attn, meta)
+        i = self.select(s, self.budget)
+        return self.apply(k, v, i)
