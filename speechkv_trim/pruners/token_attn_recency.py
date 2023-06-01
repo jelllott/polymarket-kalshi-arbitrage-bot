1597f6d20a894cf87666105756e2e98f7c6284e1
Chen Zhuoran <256720182+jelllott@users.noreply.github.com>
2023-06-01 16:42:11 +0800

    anchor protection in recency pruner

--- speechkv_trim/pruners/token_attn_recency.py
+++ speechkv_trim/pruners/token_attn_recency.py
@@ -2,7 +2,13 @@ import torch
 from .base import BasePruner
 
 class TokenAttnRecency(BasePruner):
-    def __call__(self, k, v, attn, meta):
+    def __init__(self, budget, alpha=0.7):
+        super().__init__(budget)
+        self.alpha = alpha
+    def score(self, k, v, attn, meta):
         seq = k.shape[2]
-        keep = min(self.budget, seq)
-        return k[:, :, -keep:], v[:, :, -keep:]
+        recency = torch.linspace(0, 1, seq, device=k.device)
+        score = recency
+        score[:4] = 1.0
+        score[-8:] = 1.0
+        return score
