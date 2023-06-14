d71a52a086562ef7eb32b88c2103c56d12df9077
Chen Zhuoran <256720182+jelllott@users.noreply.github.com>
2023-06-14 10:08:33 +0800

    cli skeleton with argparse

--- speechkv_trim/cli.py
+++ speechkv_trim/cli.py
@@ -1,1 +1,11 @@
-# TODO
+import argparse
+
+def main():
+    p = argparse.ArgumentParser("speechkv-trim")
+    p.add_argument("--pruner", required=True)
+    p.add_argument("--budget", type=int, default=1024)
+    args = p.parse_args()
+    print(args)
+
+if __name__ == "__main__":
+    main()
