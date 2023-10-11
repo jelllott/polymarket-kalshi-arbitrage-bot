81a1cbffc13a8fef0897dae621bf6f6717a6b619
Chen Zhuoran <256720182+jelllott@users.noreply.github.com>
2023-10-11 21:48:09 +0800

    register saliency in pruners __init__

--- speechkv_trim/pruners/__init__.py
+++ speechkv_trim/pruners/__init__.py
@@ -1,3 +1,5 @@
 from .base import BasePruner, PruneMeta
 from .token_attn_recency import TokenAttnRecency
+from .token_saliency import TokenSaliency
 from .head_l1 import HeadL1
+from .chunk_vad import ChunkVAD
