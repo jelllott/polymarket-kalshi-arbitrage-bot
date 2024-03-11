a48df3b4e6eb78f3d57b794035b7e2cf015d9611
Chen Zhuoran <256720182+jelllott@users.noreply.github.com>
2024-03-11 22:51:23 +0800

    register streaming pruner in cli + entry points

--- speechkv_trim/pruners/__init__.py
+++ speechkv_trim/pruners/__init__.py
@@ -3,3 +3,4 @@ from .token_attn_recency import TokenAttnRecency
 from .token_saliency import TokenSaliency
 from .head_l1 import HeadL1
 from .chunk_vad import ChunkVAD
+from .streaming_anchor import StreamingAnchor
